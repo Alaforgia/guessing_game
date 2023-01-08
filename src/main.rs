6c6698bd88f3db013dbcb15b5e0d28974098572f
Tony LaForgia <alforgia428@gmail.com>
2023-01-07 20:40:28 -0600

    added assoc. function comments

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,11 @@ fn main() {
 
     println!("Please input your guess.");
 
-    // mut allows the variable to be mutable. Variables are immutable in RUst by default
+    // mut allows the variable to be mutable. Variables are immutable in Rust by default.
+    //
+    // " :: " indicates that new is an associated function, which  is implemented on a type, in this
+    // case String, rather than on a particular instance of a String.
+    // "new" is common on many functions that make a new value of some kind.
     let mut guess = String::new();
 
     io::stdin().read_line(&mut guess).expect("failed to read line.");
